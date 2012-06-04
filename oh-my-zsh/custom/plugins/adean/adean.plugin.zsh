12d232e008c8c70774e413693e9149bb3210bbfd
Alexander Dean <alexander.dean@keplarllp.com>
2012-06-04 15:54:25 +0300

    Added in Zsh configuration files

--- oh-my-zsh/custom/plugins/adean/adean.plugin.zsh
+++ oh-my-zsh/custom/plugins/adean/adean.plugin.zsh
@@ -1,5 +1,15 @@
-# Some useful aliases
-alias e=emacsclient -t
-alias ec=emacsclient -c
-alias vim=emacsclient -t
-alias vi=emacsclient -t
+# Navigation aliases
+alias ll='ls -Al'
+
+# Emacs aliases
+alias e='emacsclient -t'
+alias ec='emacsclient -c'
+alias vim='emacsclient -t'
+alias vi='emacsclient -t'
+
+# Aptitude aliases
+alias aptitude="sudo aptitude"
+alias update="sudo aptitude update"
+alias upgrade="sudo aptitude upgrade"
+alias install="sudo aptitude install"
+alias remove="sudo aptitude remove"
