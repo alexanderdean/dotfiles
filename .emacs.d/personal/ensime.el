2ba3d76742da3134dad6ddc36c0e309f409cb01b
Alex Dean <alexander.dean@keplarllp.com>
2012-06-05 03:33:12 +0300

    Adding in new Emacs config files

---
+++ .emacs.d/personal/ensime.el
@@ -0,0 +1,11 @@
+;; Ensime (https://github.com/aemoncannon/ensime)-specific configuration for Emacs
+
+;; load the ensime lisp code...
+(add-to-list 'load-path "ENSIME_ROOT/elisp/")
+(require 'ensime)
+
+;; This step causes the ensime-mode to be started whenever
+;; scala-mode is started for a buffer. You may have to customize this
+;; step
+;; if you're not using the standard scala mode.
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
---
+++ .emacs.d/personal/prelude.el
@@ -0,0 +1,7 @@
+; Prelude (http://batsov.com/prelude/)-specific configuration for Emacs
+
+; Making arrow keys work again
+(prelude-restore-arrow-keys)
+
+; Default to menu-bar visible (F12 to hide again)
+(menu-bar-mode 1)
