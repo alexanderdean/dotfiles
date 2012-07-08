086f5eb856c6c1602c6ecd7441bfea61f0e7a24a
Alex Dean <alexander.dean@keplarllp.com>
2012-07-08 23:02:39 +0100

    Added default Evil mode config

---
+++ .emacs.d/personal/evil.el
@@ -0,0 +1,12 @@
+(require 'evil)
+(evil-mode 1)
+
+;;; esc quits
+
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
