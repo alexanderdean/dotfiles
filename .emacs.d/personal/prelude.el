303722244364314c9f82019c90e574e4e3391773
Alexander Dean <alexander.dean@keplarllp.com>
2012-07-08 22:47:17 +0200

    Update master

--- .emacs.d/personal/prelude.el
+++ .emacs.d/personal/prelude.el
@@ -1,7 +1,11 @@
 ; Prelude (http://batsov.com/prelude/)-specific configuration for Emacs
 
 ; Making arrow keys work again
-(prelude-restore-arrow-keys)
+(defun disable-guru-mode ()
+  (guru-mode -1)
+)
+
+(add-hook 'prelude-prog-mode-hook 'disable-guru-mode t)
 
 ; Default to menu-bar visible (F12 to hide again)
 (menu-bar-mode 1)
